680e67f23825abe6bc0f5ef397e6999c70aa4958
Saad Hesham <132556058+Saad-Hisham@users.noreply.github.com>
2023-12-29 06:34:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   <main>
 <Input/>
 <footer> Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="https://www.frontendmentor.io/profile/Saad-Hisham">Saad Hisham</a>.</footer>
+    Coded by <a href="https://3d-portofolio-seven.vercel.app/">Saad Hisham</a>.</footer>
   </main>
   </Provider>
 
@@ -25,49 +25,3 @@ export default App;
 
 
 
-
-// // import { DndContext, closestCenter } from "@dnd-kit/core";
-// // import { SortableContext, arrayMove, verticalListSortingStrategy } from "@dnd-kit/sortable";
-// // import { useState } from "react";
-// // import { Col, Container, Row } from "react-bootstrap";
-// // import { SortableItem } from "./components/SortableItem";
-
-// // function App() {
-// //   const [languages,setLanguages] = useState(["javascript","python","typescript"])
-// //  return(
-// //   <DndContext
-// //   collisionDetection={closestCenter}
-// //   onDragEnd={handleDragEnd}
-// //   >
-// //     <Container>
-// // <h3>the best programming languages</h3>
-// /* <SortableContext
-// items={languages}
-// strategy={verticalListSortingStrategy}
-// > 
-//   // {JSON.parse(localStorage.getItem("items")).map(function(language){
-//   //   return(
-//   //     <SortableItem key={language} id={language}/>
-//   //   )
-//   // })}
-
-// // </SortableContext>
-// // </Container>
-// //   </DndContext>
-// //   );
-// //   function handleDragEnd(e){
-//     console.log("drag end called")
-//     const {active,over}=e;
-//     if(active.id !== over.id){
-//       setLanguages((items)=>{
-//         const activeIndex =items.indexOf(active.id);
-//         const overIndex =items.indexOf(over.id);
-//         localStorage.setItem("items",JSON.stringify(arrayMove(items,activeIndex,overIndex)))
-//        return arrayMove(items,activeIndex,overIndex);
-      
-// //       })
-// //     }
-// //   }
-// // }
-
-// // export default App;
